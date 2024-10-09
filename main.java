5d7a8c64b440420a1831bab03a3f97dd08901537
gpaleta <128159891+gpaleta@users.noreply.github.com>
2024-10-09 16:49:28 -0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,36 +1,23 @@
 import java.util.Scanner;
-
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+    double a , b , c , delta;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("para calcular baskara digite tres valores inteiros");
+    a = sc.nextInt();
+    if (a != 0 ){
+        b = sc.nextInt();
+        c = sc.nextInt();
+        delta = Math.pow(b, 2) - 4 * a * c;
+        if (delta > 0){
+        System.out.println("existe duas raizes para esses valores apresentados");
+        }else if(delta < 0){
+            System.out.println("nao a raizes reais para esses valores apresentados");
+        }else{
+            System.out.println("existe uma raiz para esses valores apresentados");
+        };
+    };
 
-
-        int num1, num2, num3;
-        String resposta = null;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("vamos ver se a soma do primeiro e segundo numero e menor do que o terceiro");
-        System.out.println("Digite o primeiro numero");
-        num1 = sc.nextInt();
-        System.out.println("Digite o segundo numero");
-        num2 = sc.nextInt();
-        System.out.println("Digite o terceiro numero");
-        num3 = sc.nextInt();
-
-        // verifica se a soma dos valores 1 e 2 e menor que o valor 3
-        if (resultado(num1, num2) <= num3) {
-            resposta = "o terceiro numero nao e menor que a soma";
-        } else if (resultado(num1, num2) > num3) {
-            resposta = "o terceiro numero e menor que a soma";
-        }
-        System.out.println(resposta);
     }
 
-    /**
-     * Essa função soma o primeiro valor e o segundo valor
-     * @param number1 primeiro valor inteiro
-     * @param number2 primeiro valor inteiro
-     * @return retorna a soma dos valores inteiros , retorna um inteiro
-     */
-    public static int resultado(int number1,int number2) {
-        return number1 + number2;
-    }
 }
